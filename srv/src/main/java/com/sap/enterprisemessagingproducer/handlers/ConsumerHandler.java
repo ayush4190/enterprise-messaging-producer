aa06217ae4bac72d524502ae98a8bfca6e1da59d
Ayush Kumar <ayush.kumar03@sap.com>
2023-03-24 11:25:35 +0530

    Consuming S4Hana Events

--- srv/src/main/java/com/sap/enterprisemessagingproducer/handlers/ConsumerHandler.java
+++ srv/src/main/java/com/sap/enterprisemessagingproducer/handlers/ConsumerHandler.java
@@ -14,7 +14,8 @@ public class ConsumerHandler implements EventHandler{
 
 	private static final Logger logger = LoggerFactory.getLogger(ConsumerHandler.class);
 
-	@On(service = "messaging", event = "/sap/eee/iwxbe/testproducer/v1/Event/Test")
+	@On(service = "messaging", event ={ "sap/s4/beh/purchaseorder/v1/PurchaseOrder/Changed/v1", "sap/s4/beh/purchaseorder/v1/PurchaseOrder/Created/v1",
+			"sap/eee/iwxbe/testproducer/v1/Event/Test"})
 	public void listen(TopicMessageEventContext context) {
 		
 		logger.info("---------------------------Reading Payload Emitted by the Event in Same CAP based Microservice----------------------------------------------------");
